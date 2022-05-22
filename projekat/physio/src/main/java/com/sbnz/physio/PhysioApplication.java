78bd82917948c7c236f3b6675aef169186cad06b
matija <matija.matovic99@gmail.com>
2022-05-22 12:57:50 +0200

    Added some starting functions to execute so we know the project works.

--- projekat/physio/src/main/java/com/sbnz/physio/PhysioApplication.java
+++ projekat/physio/src/main/java/com/sbnz/physio/PhysioApplication.java
@@ -1,14 +1,26 @@
 package com.sbnz.physio;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+
+import com.sbnz.physio.facts.Pain;
 
 @SpringBootApplication
 public class PhysioApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(PhysioApplication.class, args);
+		
+		List<String> symptomsString = Arrays.asList(Pain.Symptom.class.getEnumConstants())
+				.stream().map(Pain.Symptom::toString).collect(Collectors.toList());
+		
+		for (String symptom : symptomsString)
+			System.out.println(symptom);
 	}
 
 }
